32c830a4ea5fdc888a97a7c87dc6d97da491b5b3
caolingcong <527891214@qq.com>
2021-08-19 17:18:12 +0800

    3.安装babel按需加载依赖

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   presets: [
     '@vue/app'
+  ],
+  /** babel-plugin-import(用于按需加载组件代码和样式的babel插件) */
+  plugins:[
+    [
+      "import",
+      {libraryName: "ant-design-vue", libraryDirectory: "es", style: true}
+    ]
   ]
 }
