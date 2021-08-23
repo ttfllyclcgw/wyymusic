9ef5464b9397ca4dcf3ef7d0811a2496bca67f89
caolingcong <527891214@qq.com>
2021-08-23 18:14:13 +0800

    6.完成排版布局

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,75 +1,110 @@
 <template>
-  <a-layout id="components-layout-demo-custom-trigger">
-    <a-layout-sider v-model="collapsed" :trigger="null" collapsible>
+  <a-layout id="components-layout-demo-side" style="min-height: 100vh">
+    <a-layout-sider v-model="collapsed" collapsible>
       <div class="logo" />
-      <a-menu
-          mode="inline" theme="dark"
-          :default-selected-keys="['1']"
-          :default-open-keys="['sub1']"
-          :style="{ height: '100%', borderRight: 0 }">
-          <a-sub-menu key="sub1">
-            <span slot="title"><a-icon type="user" />subnav 1</span>
-            <a-menu-item key="1">option1</a-menu-item>
-            <a-menu-item key="2">option2</a-menu-item>
-            <a-menu-item key="3">option3</a-menu-item>
-            <a-menu-item key="4">option4</a-menu-item>
-          </a-sub-menu>
-          <a-sub-menu key="sub2">
-            <span slot="title"><a-icon type="laptop" />subnav 2</span>
-            <a-menu-item key="5">option5</a-menu-item>
-            <a-menu-item key="6">option6</a-menu-item>
-            <a-menu-item key="7">option7</a-menu-item>
-            <a-menu-item key="8">option8</a-menu-item>
-          </a-sub-menu>
-          <a-sub-menu key="sub3">
-            <span slot="title"><a-icon type="notification" />subnav 3</span>
-            <a-menu-item key="9">option9</a-menu-item>
-            <a-menu-item key="10">option10</a-menu-item>
-            <a-menu-item key="11">option11</a-menu-item>
-            <a-menu-item key="12">option12</a-menu-item>
-          </a-sub-menu>
-        </a-menu>
+      <a-menu theme="dark" :default-selected-keys="['1']" mode="inline">
+        <a-sub-menu key="sub1">
+          <span slot="title"><a-icon type="user" /><span>发现音乐</span></span>
+          <a-menu-item key="1">推荐</a-menu-item>
+          <a-menu-item key="2">排行榜</a-menu-item>
+          <a-menu-item key="3">歌单</a-menu-item>
+          <a-menu-item key="4">主播电台</a-menu-item>
+          <a-menu-item key="5">歌手</a-menu-item>
+          <a-menu-item key="6">新碟上架</a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub2">
+          <span slot="title"><a-icon type="video-camera" /><span>我的音乐</span></span>
+          <a-menu-item key="7">创建的歌单</a-menu-item>
+          <a-menu-item key="8">收藏的歌单</a-menu-item>
+        </a-sub-menu>
+        <a-menu-item key="9">
+          <a-icon type="team" /><span>朋友</span>
+        </a-menu-item>
+        <a-menu-item key="10">
+          <a-icon type="cloud-o" /><span>商城</span>
+        </a-menu-item>
+        <a-menu-item key="11">
+          <a-icon type="pie-chart" /><span>音乐人</span>
+        </a-menu-item>
+        <a-menu-item key="12">
+          <a-icon type="appstore-o" /><span>个人中心</span>
+        </a-menu-item>
+      </a-menu>
     </a-layout-sider>
     <a-layout>
-      <a-layout-header style="background: #fff; padding: 0">
-        <a-icon
-          class="trigger"
-          :type="collapsed ? 'menu-unfold' : 'menu-fold'"
-          @click="() => (collapsed = !collapsed)"
-        />
+      <a-layout-header class="menu-aplayer">
+        <!--music：当前播放的音乐 list：播放列表 showlrc：是否显示歌词 listFolded:是否显示歌曲列表
+        listMaxHeight:播放列表面板最大高度-->
+        <aplayer class="music-aplayer" :music="musics[0]" :list="musics" :showLrc="true" :listFolded="true"
+           listMaxHeight="100px" style="margin:0"></aplayer>
       </a-layout-header>
-      <a-layout-content
-        :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '280px' }">
-        Content
+      <a-layout-content class="menu-content">
+        <!--<a-breadcrumb style="margin: 16px 0">
+          <a-breadcrumb-item>发现音乐</a-breadcrumb-item>
+          <a-breadcrumb-item>推荐</a-breadcrumb-item>
+        </a-breadcrumb>-->
+        <div :style="{ background: '#fff', minHeight: '80vh' }">
+          Bill is a cat.
+        </div>
       </a-layout-content>
+      <a-layout-footer class="menu-footer">Ant Design ©2018 Created by Ant UED</a-layout-footer>
     </a-layout>
   </a-layout>
 </template>
 
 <script>
+import aplayer from 'vue-aplayer'  //引入vue-aplayer音频播放器
 export default {
+  components:{
+    aplayer
+  },
+  mounted:{
+  },
   data() {
     return {
       collapsed: false,
+      detailForm: {
+       clickType: "",
+       title: "ajlgaljg",
+     },
+     // 音频列表
+     musics: [
+       {
+         title: "目及皆是你",
+         artist: "小蓝背心",
+         src: "歌曲文件的URL",
+         pic: "封面图片URL",
+         lrc: "歌词或歌词文件的URL",
+       },
+       {
+         title: "星辰大海",
+         artist: "黄霄雲",
+         url: "歌曲文件的URL",
+         pic: "封面图片URL",
+         lrc: "歌词或歌词文件的URL",
+       }
+     ],
     };
   },
 }
 </script>
 
 <style scoped>
-#components-layout-demo-custom-trigger .trigger {
-  font-size: 18px;
-  line-height: 64px;
-  padding: 0 24px;
-  cursor: pointer;
-  transition: color 0.3s;
-}
-#components-layout-demo-custom-trigger .trigger:hover {
-  color: #1890ff;
-}
-#components-layout-demo-custom-trigger .logo {
+#components-layout-demo-side .logo {
   height: 32px;
-  background: rgba(255, 255, 255, 0.2);
+  background: url(../assets/topbar.png?d2f76305527ad432e0bcf8ce0bfbaa4e) no-repeat 8px -19.5px;
   margin: 16px;
+}
+.menu-aplayer{
+  height: 14vh;
+  padding: 0;
+  background: #fff;
+}
+.menu-content{
+  margin: 2vh 2vh 0 2vh;
+}
+.menu-footer{
+  text-align: center;
+  padding: 0;
 }
 </style>
